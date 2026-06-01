b539354f68291c55a0db536b0dc1e1b78f0e551b
Jonathan Quintero <jonathandq21@gmail.com>
2026-06-01 16:35:39 -0500

    se agrega dist

--- Lit-lover/vite.config.ts
+++ Lit-lover/vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  // Configuración mínima para SPA
+  vite: {
+    build: {
+      outDir: "dist",   
+    }
+  }
 });

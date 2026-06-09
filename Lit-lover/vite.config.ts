4161b91955ffa41619f2524ec279b8aae17bbc6c
Jonathan Quintero <jonathandq21@gmail.com>
2026-06-08 21:11:18 -0500

    cambio

--- Lit-lover/vite.config.ts
+++ Lit-lover/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  // Le indicamos a Lovable que active el empaquetado de Nitro para Vercel
+  nitro: {
+    preset: "vercel"
+  },
   vite: {
+    // Dejamos esto como lo tenía Lovable por si sus scripts internos lo requieren
     build: {
       outDir: "dist",   
     }
